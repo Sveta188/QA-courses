24b3657638cf611d64e22846388b07c2e53899e9
hadus <haduskinasvetlana@gmail.com>
2022-01-29 14:16:40 +0300

    h

--- PycharmProjects/pythonProject/culc/text_fixture1.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-
-@pytest.fixture
-def get_user():
-    print("fixture")
--- PycharmProjects/pythonProject2/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.fixture(autouse=True) # автоюз используется для всего сьюта, а сьют в нашем случае это класс
-def fix_me():
-    print("Hi!")
-
