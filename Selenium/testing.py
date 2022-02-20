35f3340ca80cd31ee34ebb1ba102ed9c5fddf864
hadus <haduskinasvetlana@gmail.com>
2022-02-20 11:23:06 +0300

    minor fixes

--- Selenium/testing.py
+++ Selenium/testing.py
@@ -158,7 +158,7 @@ class Test_group_2:
     def login_fixture(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--no-sandbox')
-        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-extensions')
         print(self.driver_path)
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
